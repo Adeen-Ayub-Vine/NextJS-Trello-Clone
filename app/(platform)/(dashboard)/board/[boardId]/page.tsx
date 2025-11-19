a62295c4342c73cad9fa6447362ce368458269b8
Adeen Ayub Vine <133694221+Adeen-Ayub-Vine@users.noreply.github.com>
2025-11-19 09:40:17 +0500

    Create list in backend

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,7 +1,7 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-//import ListContainer from "./_components/list-container";
+import { ListContainer } from "./_components/list-container";
 
 interface BoardIdPageProps {
   params: {
@@ -9,35 +9,37 @@ interface BoardIdPageProps {
   };
 }
 
-export default async function BoardIdPage({ params }: BoardIdPageProps) {
+export const BoardIdPage = async ({ params }: BoardIdPageProps) => {
   const { orgId } = await auth();
 
   if (!orgId) {
     redirect("/select-org");
   }
 
-  // const lists = await db.list.findMany({
-  //   where: {
-  //     boardId: params.boardId,
-  //     board: {
-  //       orgId,
-  //     },
-  //   },
-  //   include: {
-  //     cards: {
-  //       orderBy: {
-  //         order: "asc",
-  //       },
-  //     },
-  //   },
-  //   orderBy: {
-  //     order: "asc",
-  //   },
-  // });
+  const lists = await db.list.findMany({
+    where: {
+      boardId: params.boardId,
+      board: {
+        orgId,
+      },
+    },
+    include: {
+      cards: {
+        //card order
+        orderBy: {
+          order: "asc",
+        },
+      },
+    },
+    //list order
+    orderBy: {
+      order: "asc",
+    },
+  });
 
   return (
     <div className="p-4 h-full overflow-x-auto">
-      {/* <ListContainer boardId={params.boardId} data={lists} /> */}
+      <ListContainer boardId={params.boardId} data={lists} />
     </div>
   );
-}
+};
